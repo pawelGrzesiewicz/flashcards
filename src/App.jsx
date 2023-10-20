6d1c91e19bc77b04c073b49501b5e7ecfd97f3ed
Paweł Grzesiewicz <p.grzesiewicz@gmail.com>
2023-10-20 21:13:47 +0200

    add view docks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import Decks from "./components/decks/Decks.jsx";
+import { useState } from "react";
 
-function App() {
+
   return (
     <>
       <Decks />
--- src/components/decks/Decks.jsx
+++ src/components/decks/Decks.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { getDecks } from "../../api/api.js";
+
 function Decks() {
-  return <div>Decks Component</div>;
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    getDecks(controller.signal).then((data) => setDecks(data));
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Decks</h1>
+      {decks.map(({ id, name, cards }) => (
+        <div
+          key={id}
+          style={{
+            border: "1px solid dark grey",
+            borderRadius: 4,
+            marginBottom: 10,
+            padding: 10,
+          }}
+        >
+          <h2>
+            {name}, <span>{cards}</span>
+          </h2>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Decks;
